fcd2080a4e4cce503281977fb9cb2330f4031950
DESKTOP-EPSD9EJ\Welcome <ha.hohcr4330@hcmut.edu.vn>
2024-09-19 22:47:43 +0700

    no message

--- STM32_Lab1/Core/Src/exercise3.c
+++ STM32_Lab1/Core/Src/exercise3.c
@@ -29,29 +29,29 @@ void set_led_group(int direction, int led_color) {
 
         case 1: // East (LED_5, LED_6, LED_7)
             if (led_color == LED_RED)
-                HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_5_Pin, GPIO_PIN_SET);
             else if (led_color == LED_YELLOW)
-                HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_6_Pin, GPIO_PIN_SET);
             else if (led_color == LED_GREEN)
-                HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_7_Pin, GPIO_PIN_SET);
             break;
 
         case 2: // South (LED_9, LED_10, LED_11)
             if (led_color == LED_RED)
-                HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_9_Pin, GPIO_PIN_SET);
             else if (led_color == LED_YELLOW)
-                HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_10_Pin, GPIO_PIN_SET);
             else if (led_color == LED_GREEN)
-                HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_11_Pin, GPIO_PIN_SET);
             break;
 
         case 3: // West (LED_3, LED_2, LED_1)
             if (led_color == LED_RED)
-                HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_3_Pin, GPIO_PIN_SET);
             else if (led_color == LED_YELLOW)
-                HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_2_Pin, GPIO_PIN_SET);
             else if (led_color == LED_GREEN)
-                HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_1_Pin, GPIO_PIN_SET);
             break;
 
         default:
