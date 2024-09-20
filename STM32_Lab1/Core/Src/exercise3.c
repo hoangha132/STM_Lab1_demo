3d0d476751ba7a33bfceeccaea730181cde3b1dd
DESKTOP-EPSD9EJ\Welcome <ha.hohcr4330@hcmut.edu.vn>
2024-09-20 08:35:14 +0700

    update ex3

--- STM32_Lab1/Core/Src/exercise3.c
+++ STM32_Lab1/Core/Src/exercise3.c
@@ -5,9 +5,21 @@ int counter3 = 0;
 
 // LED Pin and Port configuration
 void init_exercise3() {
-    for (int i = 0; i < 12; i++) {
-        HAL_GPIO_WritePin(GPIOB, (1 << i), GPIO_PIN_RESET);
-    }
+//    for (int i = 0; i < 12; i++) {
+//        HAL_GPIO_WritePin(GPIOB, (1 << i), GPIO_PIN_RESET);
+//    }
+    HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, RESET);
+    HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, RESET);
+    HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, RESET);
+    HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, RESET);
+    HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, RESET);
+    HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, RESET);
+    HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, RESET);
+    HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, RESET);
+    HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, RESET);
+    HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, RESET);
+    HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, RESET);
+    HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, RESET);
 }
 
 void set_led_group(int direction, int led_color) {
@@ -88,7 +100,7 @@ void exercise3_run() {
         }
     }
 
-    if (counter3 >= 10) {  // Reset the counter after the full cycle
+    if (counter3 >= 10) {
         counter3 = 0;
     }
 }
